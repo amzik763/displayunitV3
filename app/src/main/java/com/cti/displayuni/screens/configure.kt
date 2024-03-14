734760fa2d190e2b8b8865769a48189f0bee1750
JYOTI PANDEY <jyotip9352@gmail.com>
2024-03-14 18:06:59 +0530

    Configure Screen

---
+++ app/src/main/java/com/cti/displayuni/screens/configure.kt
@@ -0,0 +1,10 @@
+package com.cti.displayuni.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Tablet", device = "spec:width=1920px,height=1080px,dpi=160,isRound=false,orientation=landscape", showBackground = true, showSystemUi = true)
+@Composable
+fun Configure(){
+
+}
